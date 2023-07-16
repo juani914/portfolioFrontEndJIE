95839c0e8cf6660a2f0ceb8837f62327e52dae35
juani914 <juanielizari@gmail.com>
2023-07-15 23:01:47 -0300

    cambios persona acerca

--- src/app/componentes/acerca-mi/acerca-mi.component.ts
+++ src/app/componentes/acerca-mi/acerca-mi.component.ts
@@ -18,7 +18,7 @@ export class AcercaMiComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    const id = 4;
+    const id = 1;
     this.personaService.getPersona(id).subscribe(data => {
       this.persona = data;
     }, error => {
